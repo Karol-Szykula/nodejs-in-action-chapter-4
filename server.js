96b3b36a4858e507624b6f83edd91f0b826b9f0e
Karol-Szykula <karol.szykula@wp.pl>
2018-12-16 15:17:06 +0100

    Add redirection to google.pl

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const http = require('http')
 
 const server = http.createServer(function (req, res) {
 
-    let body = 'Hello world'
+    const url = 'http://google.pl'
+
+    let body = '<p>Redirection to <a href="' + url + '">' + url + '</a></p>'
+    res.setHeader('Location', url)
     res.setHeader('Content-Length', body.length)
-    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Content-Type', 'text/html')
+
+    res.statusCode = 302
     res.end(body)
 })
 
