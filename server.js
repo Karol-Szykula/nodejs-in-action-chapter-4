63bca6af653eccebdff1b988c007b84bf0e69d24
Karol-Szykula <karol.szykula@wp.pl>
2018-12-16 15:12:05 +0100

    Change body type

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http')
 
 const server = http.createServer(function (req, res) {
-    res.write('Hello World')
-    res.end()
+
+    let body = 'Hello world'
+    res.setHeader('Content-Length', body.length)
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(body)
 })
 
 server.listen(3000)
