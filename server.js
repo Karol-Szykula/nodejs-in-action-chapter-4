e2fd2f12467bac51f1c83d39cc581fd725de738d
Karol-Szykula <karol.szykula@wp.pl>
2018-12-19 19:49:40 +0100

    Define content length header in bytes

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ const server = http.createServer(function (req, res) {
             })
             break
         case 'GET':
-            items.forEach(function (item, i) {
-                res.write(i + ') ' + item + '\n')
-            })
+            const body = items.map((item, i) => {
+                return i + ')' + item
+            }).join('\n')
+            res.setHeader('Content-Length', Buffer.byteLength(body))
+            res.setHeader('Content-Type', 'text/plain; charset="utf-8"')
             res.end()
             break
 
