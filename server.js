d82e27753ce5b56c5c85806f5f89f21153c130dc
Karol-Szykula <karol.szykula@wp.pl>
2018-12-19 20:02:01 +0100

    Add Delete task method

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const items = []
 
 const server = http.createServer(function (req, res) {
     switch (req.method) {
+
         case 'POST':
             let item = ''
             req.setEncoding('utf8')
@@ -18,6 +19,7 @@ const server = http.createServer(function (req, res) {
                 res.end()
             })
             break
+
         case 'GET':
             const body = items.map((item, i) => {
                 return i + ')' + item
@@ -25,6 +27,22 @@ const server = http.createServer(function (req, res) {
             res.setHeader('Content-Length', Buffer.byteLength(body))
             res.setHeader('Content-Type', 'text/plain; charset="utf-8"')
             res.end()
+            break
+
+        case 'DELETE':
+            let path = url.parse(req.url).pathname
+            let i = parseInt(path.slice(1), 10)
+
+            if (isNaN(i)) {
+                res.statusCode = 400
+                res.end('Wrong identifier of an element')
+            } else if (!items[i]) {
+                res.statusCode = 400
+                res.end('Element has not been found')
+            } else {
+                items.splice(i, 1)
+                res.end('OK\n')
+            }
             break
 
         default:
