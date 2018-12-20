b8e7166c41a65dd12c2330b12915dde11c38686c
Karol-Szykula <karol.szykula@wp.pl>
2018-12-20 10:53:19 +0100

    Move to chapter - 4.3.1 - create static file server

--- server.js
+++ server.js
@@ -1,89 +1,21 @@
-'use strict'
 const http = require('http')
-const url = require('url')
+const parse = require('url').parse
+const join = require('path').join
+const fs = require('fs')
+const root = __dirname
 
-let items = ['Buy a book', 'Read a book', 'Buy a milk']
+const server = http.createServer((req, res) => {
+    const url = parse(req.url)
+    const path = join(root, url.pathname)
+    const stream = fs.createReadStream(path)
 
-const server = http.createServer(function (req, res) {
-    switch (req.method) {
+    stream.on('data', function (chunk) {
+        res.write(chunk)
+    })
 
-        case 'POST':
-            var item = ''
-            req.setEncoding('utf8')
-
-            req.on('data', function (chunk) {
-                item += chunk
-            })
-
-            req.on('end', function () {
-                items.push(item)
-                res.end()
-            })
-            break
-
-        case 'PUT':
-            var item = ''
-            req.setEncoding('utf8')
-
-            req.on('data', function (chunk) {
-                item += chunk
-            })
-
-            var path = url.parse(req.url).pathname
-            var i = parseInt(path.slice(1), 10)
-
-            if (isNaN(i)) {
-                res.statusCode = 400
-                res.end('Wrong identifier of an element')
-            } else if (!items[i]) {
-                res.statusCode = 400
-                res.end('Element has not been found')
-            } else {
-                req.on('end', function () {
-                    var oldItem = items[i]
-                    var newItem = oldItem.concat(item)
-
-                    var rearOfOldArray = items.slice(i + 1, items.length)
-
-                    var newItems = items.slice(0, i)
-                    newItems.push(newItem)
-                    newItems = newItems.concat(rearOfOldArray)
-
-                    items = newItems
-                    res.end('OK\n')
-                })
-            }
-            break
-
-        case 'GET':
-            const body = items.map(function (item, i) {
-                return i + ') ' + item
-            }).join('\n')
-            res.setHeader('Content-Length', Buffer.byteLength(body))
-            res.setHeader('Content-Type', 'text/plain charset="utf-8"')
-            res.end(body)
-            break
-
-        case 'DELETE':
-            var path = url.parse(req.url).pathname
-            var i = parseInt(path.slice(1), 10)
-
-            if (isNaN(i)) {
-                res.statusCode = 400
-                res.end('Wrong identifier of an element')
-            } else if (!items[i]) {
-                res.statusCode = 400
-                res.end('Element has not been found')
-            } else {
-                items.splice(i, 1)
-                res.end('OK\n')
-            }
-            break
-
-        default:
-            return
-    }
-
+    stream.on('end', () => {
+        res.end()
+    })
 })
 
 server.listen(3000)
